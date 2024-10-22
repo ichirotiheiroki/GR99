1e6475888a92b89d2b9f37ea722a68dea9b6c523
ichirotiheiroki <deadichi@gmail.com>
2024-10-22 15:07:36 +0400

    script

---
+++ script.js
@@ -0,0 +1,46 @@
+document.getElementById('submitForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const url = new URL(window.location.href);
+    const urlParams = new URLSearchParams(url.search);
+    const utmSource = urlParams.get('utm_source');
+    const utmCampaign = urlParams.get('utm_campaign');
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+
+    const baseURL = 'https://someserver'
+
+
+    const data = {
+        customer: {
+            name: name,
+            phone: phone,
+            email: email,
+        },
+        channelId: utmSource,
+        marketingCampaignId: utmCampaign,
+        gymId: "",
+        comment: "",
+        company: ""
+    }
+
+
+    fetch(`${baseURL}/events/callback`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(result => {
+            console.log('Success:', result);
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+
+        });
+});
